906b56bf8d29db23bad9d89874b2d5fec206b684
dipenwow <dipenshakya19@gmail.com>
2023-01-04 09:19:00 +0545

    custom and auth scaffolding authentication with session flash message and validation with form reuest

---
+++ app/Http/Controllers/CustomController.php
@@ -0,0 +1,92 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\custom;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Session;
+
+
+
+
+
+
+
+
+
+
+class CustomController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('custom.login');
+    }
+
+    public function postLogin(custom $request)
+    {
+        
+        $validateData = $request->validated();
+        $credintials = $request->only('email', 'password');
+        if (Auth::attempt($credintials))
+        {
+            return redirect()->intended('custom/dashboard')->withSuccess('success','Logged in successfully');
+        }
+            return redirect("custom/login")->withSuccess('Invalid Credintials');
+
+    }
+
+   public function registration()
+   {
+    return view('custom/register');
+   }
+
+   public function postRegister(custom $request)
+   {
+    $validatedData = $request->validated();
+    /**
+     * $user =new User;
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = $request->password;
+     */
+
+    $data = $request->all();
+    $check = $this->create($data);
+
+    return redirect("custom/dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+
+    public function create(array $data)
+    
+        {
+            return User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password'])
+              ]);
+        }
+    
+
+   public function dashboard()
+   {    
+       if(Auth::check()){
+           return view('custom/dashboard');
+           
+       }
+ 
+       return redirect("custom/login")->withSuccess('Opps! You do not have access');
+   }
+
+   public function logout()
+   {
+    Session::flush();
+    Auth::logout();
+
+    return Redirect('custom/login');
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CustomController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +27,15 @@ Route::get('/', function () {
 Route::get('/db', function () {
     return view('dbconn');
 });
+ 
 
-//display
+
+   //display
 Route::get('/post',[PostController::class,'show']);
 //Route::get('/post/create',[PostController::class,'show'])->name('post');
 
 //create
-Route::view('/create','create');
+Route::view('home/create','create');
 Route::post('store',[PostController::class,'store']);
 //Delete
 Route::delete('post/{id}',[PostController::class,'destroy'])->name('destroy');
@@ -38,4 +47,26 @@ Route::PATCH('post/update/{id}',[PostController::class,'update'])->name('update'
 
 
 
+
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//custom authentication routes
+
+Route::get('custom/login',[CustomController::class, 'index']);
+Route::post('login',[CustomController::class, 'postLogin']);
+Route::get('registration',[CustomController::class, 'registration']);
+Route::post('custom-Registration',[CustomController::class, 'postRegister']);
+
+Route::get('custom/dashboard',[CustomController::class, 'dashboard']);
+//Route::view('login','custom/login');
+//Route::view('register','custom/register');
+Route::get('logout', [CustomController::class, 'logout']);
 
